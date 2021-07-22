46a4dbfc3f3d7b0a348f9012c0955819c0e95adf
Alek Babich <ababich@roadrunner.com>
2021-07-21 20:31:00 -0400

    Protecting from entering date in the past

--- script.js
+++ script.js
@@ -4,23 +4,32 @@ const minutesElement = document.getElementById("minutes");
 const secondsElement = document.getElementById("seconds");
 const initialDateElement = document.getElementById("input-date");
 
-var desiredDate = prompt("Please enter a date to countdown to", "01 Jan 2022");
+var desiredDate = prompt("Please enter a date to countdown to", "01/01/2022");
 
 function countdown() {
-  const newYearsDate = new Date(desiredDate);
+  const futureDate = new Date(desiredDate);
   const currentDate = new Date();
 
+  if (futureDate < currentDate) {
+    window.alert("Please enter a date in the future");
+    refresh();
+  }
+
   // 86400000 Milliseconds in a day
-  const days = Math.floor((newYearsDate - currentDate) / 86400000);
-  const hours = Math.floor(((newYearsDate - currentDate) % 86400000) / 3600000);
-  const minutes = Math.floor(((newYearsDate - currentDate) % 3600000) / 60000);
-  const seconds = Math.floor(((newYearsDate - currentDate) % 60000) / 1000);
+  const days = Math.floor((futureDate - currentDate) / 86400000);
+  const hours = Math.floor(((futureDate - currentDate) % 86400000) / 3600000);
+  const minutes = Math.floor(((futureDate - currentDate) % 3600000) / 60000);
+  const seconds = Math.floor(((futureDate - currentDate) % 60000) / 1000);
 
   daysElement.innerHTML = days;
   hoursElement.innerHTML = formatTime(hours);
   minutesElement.innerHTML = formatTime(minutes);
   secondsElement.innerHTML = formatTime(seconds);
   initialDateElement.innerHTML = desiredDate;
+}
+
+function refresh() {
+  location.reload();
 }
 
 function formatTime(time) {
